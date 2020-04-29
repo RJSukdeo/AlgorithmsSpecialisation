0185455992ba55318169ccbe01fdf79e96333f66
ryan <RJSukdeo@gmail.com>
2020-04-29 10:10:41 -0400

    Added Assessment file.

---
+++ src/main/java/CourseSolutions/Course3/Problem3_CompressionDynamicAlgorithms/AssessmentC3P3.java
@@ -0,0 +1,28 @@
+package CourseSolutions.Course3.Problem3_CompressionDynamicAlgorithms;
+
+import CourseSolutions.ICourseSolution;
+
+import java.io.FileNotFoundException;
+
+public final class AssessmentC3P3 implements ICourseSolution {
+
+    @Override
+    public void run() throws FileNotFoundException {
+
+    }
+
+    @Override
+    public String getProblemDescription() {
+        return "Huffman codes and Maximum Weighted Independent Sets.";
+    }
+
+    @Override
+    public short getCourseNumber() {
+        return 3;
+    }
+
+    @Override
+    public short getProblemNumber() {
+        return 3;
+    }
+}
